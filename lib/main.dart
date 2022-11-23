058b0ff559ed7b1f625f204f70729d3d178344c5
Reyhan Gedik <334480@student.mboutrecht.nl>
2022-11-23 11:53:06 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/pages/products_overview_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductOverviewPage(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('MyShop'),
+//       ),
+//       body: Center(
+//         child: Text('Let\'s build a shop!'),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+// dart auto impo - ayarlardan kapattık.
